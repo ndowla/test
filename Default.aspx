a976e6af989eef9d6fb7a91ac476f91afe55b4a1
Nadia Dowla <nadfad@gmail.com>
2015-09-03 11:24:49 -0400

    test

---
+++ Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+   <asp:label runat="server" ID="lbl1"></asp:label>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var path = @"C:\Mango.xlsx";
+        var connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;";
+        var conn = new OleDbConnection(connStr);
+        conn.Open();
+
+        try
+        {
+            var MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", conn);
+            MyCommand.TableMappings.Add("Table", "TestTable");
+            var DtSet = new System.Data.DataSet();
+            MyCommand.Fill(DtSet);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < DtSet.Tables[0].Rows.Count; i++)
+            {
+
+                var firstname = DtSet.Tables[0].Rows[i].ItemArray[1];
+                var lastname = DtSet.Tables[0].Rows[i].ItemArray[0];
+                var jobname = DtSet.Tables[0].Rows[i].ItemArray[2];
+                var startdate = DtSet.Tables[0].Rows[i].ItemArray[5];
+
+                sb.Append("select * from employee where lastname = '" + lastname + "' and firstname = '" + firstname + "' and EmployeeHRID =  '' and jobgroupid in (select jobgroupid from jobgroup where name = '" + jobname + "') and hiredate = '" + startdate + "' <br />");
+
+
+            }
+
+            lbl1.Text = sb.ToString();
+
+
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message.ToString());
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
